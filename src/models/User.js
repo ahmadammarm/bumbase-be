e1311c5a02a29379abfe8252833c028517597086
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2025-06-29 12:25:37 +0700

    feat(models): add user model

---
+++ src/models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    matches: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address'],
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    matches: [
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+      'Password must contain at least one uppercase letter, one lowercase letter, and one number',
+    ],
+  },
+  // 1. superadmin, 2. admin, 3. user
+  role: {
+    type: Number,
+    default: 3,
+  },
+});
+
+const User = mongoose.model('user', userSchema);
+module.exports = User;
