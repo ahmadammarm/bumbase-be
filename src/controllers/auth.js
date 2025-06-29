c066a08b6f4a8bbab1edac85f30030614aab30ab
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2025-06-29 12:39:10 +0700

    feat(auth): add route for the sign up

---
+++ src/controllers/auth.js
@@ -0,0 +1,32 @@
+const {User} = require('../models');
+
+const signup = async (request, response, next) => {
+  try {
+    const {name, email, password, role} = request.body;
+
+    const newUser = new User({
+      name,
+      email,
+      password,
+      role,
+    });
+
+    await newUser.save();
+
+    response.status(201).json({
+      message: 'User created successfully',
+      user: {
+        id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        role: newUser.role,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  signup,
+};
