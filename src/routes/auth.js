b9472e6064e57c584b5e7d9cffc25da16943dc82
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2025-07-07 16:31:19 +0700

    feat(auth): implement the jwt middleware for authorization

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -11,12 +11,21 @@ const {
 const validate = require('../utils/validators/validate');
 
 const {authController} = require('../controllers');
+const isAuthenticated = require('../middlewares/isAuthenticated');
 
+// sign up and sign in
 router.post('/auth/signup', validateSignup, validate, authController.signup);
 router.post('/auth/signin', validateSignin, validate, authController.signin);
+
+// user verification
 router.post('/auth/send-verification-email', emailValidator, validate, authController.verifyCode);
 router.post('/auth/verification', verifyUserValidator, validate, authController.verifyUser);
+
+// recover password
 router.post('/auth/forgot-password-code', emailValidator, validate, authController.forgotPasswordCode);
 router.post('/auth/recover-password', recoverPasswordValidator, validate, authController.recoverPassword);
+
+// change password
+router.put('/auth/change-password', isAuthenticated, authController.changePassword);
 
 module.exports = router;
