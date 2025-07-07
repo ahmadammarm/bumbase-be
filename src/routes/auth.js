afa21cdc073f5ee3678d8a0c99362af657129941
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2025-07-07 20:27:24 +0700

    feat(auth): add rate limit middleware for sign in, sign up, and forgot password endpoints

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -13,17 +13,18 @@ const validate = require('../utils/validators/validate');
 
 const {authController} = require('../controllers');
 const isAuthenticated = require('../middlewares/isAuthenticated');
+const rateLimiter = require('../middlewares/rateLimiter');
 
 // sign up and sign in
-router.post('/auth/signup', validateSignup, validate, authController.signup);
-router.post('/auth/signin', validateSignin, validate, authController.signin);
+router.post('/auth/signup', rateLimiter, validateSignup, validate, authController.signup);
+router.post('/auth/signin', rateLimiter, validateSignin, validate, authController.signin);
 
 // user verification
 router.post('/auth/send-verification-email', emailValidator, validate, authController.verifyCode);
 router.post('/auth/verification', verifyUserValidator, validate, authController.verifyUser);
 
 // recover password
-router.post('/auth/forgot-password-code', emailValidator, validate, authController.forgotPasswordCode);
+router.post('/auth/forgot-password-code', rateLimiter, emailValidator, validate, authController.forgotPasswordCode);
 router.post('/auth/recover-password', recoverPasswordValidator, validate, authController.recoverPassword);
 
 // change password
