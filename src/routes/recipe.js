78e7e089cfa5ed93f0e433f3a37f84b9e1a1ad6c
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2025-07-11 16:29:44 +0700

    feat(recipe): finish all recipe endpoints

--- src/routes/recipe.js
+++ src/routes/recipe.js
@@ -4,15 +4,22 @@ const { recipeController } = require('../controllers');
 const isAuthenticated = require('../middlewares/isAuthenticated');
 const isAuthor = require('../middlewares/isAuthor');
 const isAdmin = require('../middlewares/isAdmin');
+const { addRecipeValidator } = require('../utils/validators/recipe');
+const validate = require('../utils/validators/validate');
 const router = express.Router();
 
 // get all recipes for all users
 router.get('/recipe', recipeController.getAllRecipes);
+router.get('/recipe/:id', recipeController.getRecipeById);
 
 // author role routes
-router.post('/author/recipe', isAuthenticated, isAuthor, recipeController.addRecipe);
+router.post('/author/recipe', isAuthenticated, isAuthor, addRecipeValidator, validate, isAuthor, recipeController.addRecipe);
+router.put('/author/recipe/:id', isAuthenticated, isAuthor, addRecipeValidator, validate, recipeController.updateRecipe);
+router.delete('/author/recipe/:id', isAuthenticated, isAuthor, recipeController.deleteRecipe);
 
 // admin role routes
 router.post('/admin/recipe', isAuthenticated, isAdmin, recipeController.addRecipe);
+router.put('/admin/recipe/:id', isAuthenticated, isAdmin, addRecipeValidator, validate, recipeController.updateRecipe);
+router.delete('/admin/recipe/:id', isAuthenticated, isAdmin, recipeController.deleteRecipe);
 
 module.exports = router;
